e36e417a8793b5e6d364b1dae5e665f5b812a95e
Chuong Dang <chuong.php@gmail.com>
2018-05-22 13:44:03 +0700

    Fix Dockerfile for local running

--- docker/fpm/init.sh
+++ docker/fpm/init.sh
@@ -7,15 +7,15 @@ echo 'alias ll="ls -lha"' >> ~/.bashrc
 if [ ! -d "/var/www/astro" ]; then
     cd /var/www
     git clone https://github.com/chuongdang/duty-manager.git astro
+
+    cd /var/www/astro
+
+    aws s3 cp s3://nksg/source/astro-vendor.tar.gz astro-vendor.tar.gz --profile default --region=ap-southeast-1
+
+    tar xzvf astro-vendor.tar.gz
+
+    rm astro-vendor.tar.gz
 fi
-
-cd /var/www/astro
-
-aws s3 cp s3://nksg/source/astro-vendor.tar.gz astro-vendor.tar.gz --profile default --region=ap-southeast-1
-
-tar xzvf astro-vendor.tar.gz
-
-rm astro-vendor.tar.gz
 
 cd /var/www/astro/public/dev
 
